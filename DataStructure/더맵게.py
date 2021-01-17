aa03649097f46dc6a78dd78f62713b57ed636f7e
madilyn <lollin0@naver.com>
2021-01-17 16:43:51 +0900

    ADD 프로그래머스 - 더 맵게(힙)

---
+++ DataStructure/더맵게.py
@@ -0,0 +1,34 @@
+import heapq
+
+def solution(scoville, K):
+    heap = []
+    for x in scoville:
+        heapq.heappush(heap, x)
+    count = 0
+    
+    while heap[0] < K:
+        if len(heap) == 1:
+            count = -1
+            break
+        heapq.heappush(heap, (heapq.heappop(heap) + heapq.heappop(heap) * 2))
+        count = count + 1
+    return count
+
+print(solution([1, 0], 1))
+
+
+'''
+def solution(scoville, K):
+    count = 0
+    
+    scoville.sort(reverse = True)
+    while scoville[len(scoville)-1] < K:
+        if len(scoville) == 1:
+            count = -1
+            break
+        scoville.append(scoville.pop() + scoville.pop() * 2)
+        scoville.sort(reverse = True)
+        count = count + 1
+        
+    return count
+'''
